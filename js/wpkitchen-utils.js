fd47c00fb062b2cdee85c557adfdde661fae8384
David V. Krukov <david.v.krukov@gmail.com>
2013-02-08 20:12:44 +0200

    Replace on change featured

--- js/wpkitchen-utils.js
+++ js/wpkitchen-utils.js
@@ -6,12 +6,16 @@ jQuery(document).ready(function($){
 			if(featured.length>0){
 				$(document).data('current_post_thumbnail',$(featured).attr('src'));
 				var k=$('#wpk_metabox_container ul li.wpk_fb_icon_row').size();
-				var img='<li class="wpk_fb_icon_row">';
+				var img='<li class="wpk_fb_icon_row" id="wpk_'+k+'_featured">';
 				img+='<div class="wpk_fb_icon" style="background:url('+timthumbPath+'?src='+$(featured).attr('src')+'&w=50&h=50&q=80&zc=1) no-repeat center center"></div>';
 				img+='<input class="wpk_fb_icon_checkbox" id="wpk_fb_image_'+k+'" name="wpk_fb_image[]" type="checkbox" value="'+$(featured).attr('src')+'::'+$(featured).attr('alt')+'"/><label for="wpk_fb_image_'+k+'">'+$(featured).attr('alt');
 				img+='</label>';
 				img+='</li>';
-				$('#wpk_metabox_container ul').append(img);
+				if($('#wpk_metabox_container ul li#wpk_'+k+'_featured').size()>0){
+					$('#wpk_metabox_container ul li#wpk_'+k+'_featured').replace(img);
+				}else{
+					$('#wpk_metabox_container ul').append(img);
+				}
 			}
 		}
 	});
