2f6ecfc940e9a7a65dd2d291a01ad7b9f9070d43
David V. Krukov <david.v.krukov@gmail.com>
2013-02-12 02:10:48 +0200

    JS fix

--- js/wpkitchen-utils.js
+++ js/wpkitchen-utils.js
@@ -51,14 +51,14 @@ jQuery(document).ready(function($){
 				if(featured.length>0){
 					$(document).data('current_post_thumbnail',$(featured).attr('src'));
 					var k=$('#wpk_metabox_container ul li.wpk_fb_icon_row').size();
-					var img='<li class="wpk_fb_icon_row" id="wpk_featured">';
 					var html='<div class="wpk_fb_icon"><img src="'+$(featured).attr('src')+'" onload="wpk_buildIcon(event)"/></div>';
 					html+='<input class="wpk_fb_icon_checkbox" id="wpk_fb_image_'+k+'" name="wpk_fb_image[]" type="checkbox" value="'+$(featured).attr('src')+'::'+$(featured).attr('alt')+'"/><label for="wpk_fb_image_'+k+'">'+$(featured).attr('alt');
 					html+='</label>';
-					img+=html+'</li>';
 					if($('#wpk_metabox_container ul li#wpk_featured').length>0){
+						
 						$('#wpk_metabox_container ul li#wpk_featured').html(html);
 					}else{
+						html='<li class="wpk_fb_icon_row" id="wpk_featured">'+html+'</li>';
 						$('#wpk_metabox_container ul').append(img);
 					}
 				}
